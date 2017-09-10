543d879d830ea659f93d901077012e29199ab04d
Alexey Sergeev <sergeev.a@nekidaem.ru>
2017-09-10 16:04:26 +0800

    DLL_ENTRY

--- max_dump/storage_parser.py
+++ max_dump/storage_parser.py
@@ -4,7 +4,7 @@
 import io
 import json
 import sys
-from enum import Enum
+from enum import IntEnum, unique, auto
 from struct import unpack
 from pprint import pprint
 
@@ -19,19 +19,52 @@ from .utils import INT_S, SHORT_S, read_int
 HEADER_LENGTH = INT_S + SHORT_S
 
 
-class StorageType(Enum):
-    CONTAINER = "CStorageContainer"
-    VALUE = "CStorageValue"
+DEBUG = False
+#  DEBUG = True
 
-    #  def __repr__(self):
-        #  return "{!r}".format(self.value)
+def print(*args, **kwargs):
+    if DEBUG:
+        __builtins__['print'](*args, **kwargs)
 
 
-class EnumEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, Enum):
-            return obj.value
-        return super().default(obj)
+@unique
+class StorageType(IntEnum):
+    # Container types 100-199
+    CONTAINER = 100
+    DLL_ENTRY = auto()
+    # Value types 200-299
+    VALUE = 200
+    DLL_DESCRIPTION = auto()
+    DLL_NAME = auto()
+
+    def is_value(self):
+        return self.VALUE <= self
+
+    def is_container(self):
+        return self.CONTAINER <= self < self.VALUE
+
+    def __repr__(self):
+        return self.name
+
+
+def utf_16_decode(val):
+    return bytes.decode(val, 'utf-16')
+
+
+KNOWN_TYPES = {
+    "0x2037": {
+        "storage_type": StorageType.DLL_NAME,
+        "decoder": utf_16_decode,
+    },
+    "0x2038": {
+        "storage_type": StorageType.DLL_ENTRY,
+        "decoder": lambda _: None,
+    },
+    "0x2039": {
+        "storage_type": StorageType.DLL_DESCRIPTION,
+        "decoder": utf_16_decode,
+    },
+}
 
 
 @attr.s(slots=True)
@@ -42,6 +75,7 @@ class Header:
     idn = attr.ib()
     length = attr.ib()
     storage_type = attr.ib()
+    storage_type_name = attr.ib()
 
 
 @attr.s(slots=True)
@@ -50,6 +84,19 @@ class StorageValue:
     """
     header = attr.ib()
     value = attr.ib()
+    parsed = attr.ib()
+
+    @property
+    def value_bytes(self):
+        return bytes.fromhex(self.value)
+
+    @parsed.default
+    def parsed_default(self):
+        if self.header.idn not in KNOWN_TYPES:
+            return None
+        idn = self.header.idn
+        decoder = KNOWN_TYPES[idn]["decoder"]
+        return decoder(self.value_bytes)
 
 
 @attr.s(slots=True)
@@ -60,21 +107,51 @@ class StorageContainer:
     """
     header = attr.ib()
     childs = attr.ib()
+    count = attr.ib()
+
+    @count.default
+    def count_default(self):
+        return len(self.childs)
 
 
-def read_idn(stream):
+class StorageException(Exception):
+    pass
+
+
+nest = 0
+
+def pad():
+    global nest
+    return " " * nest * 2
+
+
+def read_idn(stream) -> hex:
     """Read an identifier of a chunk.
 
     An identifier is an unsigned short integer.
     """
-    return unpack('H', stream.read(SHORT_S))[0]
+    b = stream.read(SHORT_S)
+    print(pad(), hexdump.dump(b))
+    return hex(unpack('H', b)[0])
+
+
+def read_int(stream):
+    b = stream.read(INT_S)
+    print(pad(), hexdump.dump(b))
+    return unpack('i', b)[0]
+    #  return unpack('i', stream.read(INT_S))[0]
 
 
 def read_header(stream):
-    """Return id, length, type of the chunk (CStorageContainer or CStorageValue)
+    """Return id, length, type of the chunk.
     """
     idn = read_idn(stream)
-    length = read_int(stream) - HEADER_LENGTH
+    length = read_int(stream)
+    if length == 0:
+        raise StorageException(
+                "Extended header length is not yet supported: {}".format(idn)
+        )
+    length -= HEADER_LENGTH
     # the msb is a flag that helpfully lets us know if the chunk itself
     # contains more chunks, i.e. is a container
     sign_bit = 1 << 31
@@ -85,46 +162,65 @@ def read_header(stream):
         length &= ~sign_bit & byte_mask
     else:
         storage_type = StorageType.VALUE
-    return Header(idn=idn, length=length, storage_type=storage_type)
+    # Specify type if `idn' is known.
+    if idn in KNOWN_TYPES:
+        storage_type = KNOWN_TYPES[idn]["storage_type"]
+    return Header(idn, length, storage_type, storage_type.name)
 
 
 def read_value(stream, length):
     val = stream.read(length)
-    return hexdump.dump(val)
+    s = val.hex()
+    print(pad(), s)
+    return s
 
 
 def read_container(stream, length):
     """Parse the storage stream in the max container file.
     """
+    global nest
+    nest += 1
     childs = []
     start = stream.tell()
     consumed = 0
     while consumed < length:
         header = read_header(stream)
         child = None
-        if header.storage_type == StorageType.CONTAINER:
+        print(pad(), header.storage_type_name)
+        if header.storage_type.is_container():
+        #  if header.storage_type == StorageType.CONTAINER:
+            print(pad(), "container")
             nodes = read_container(stream, header.length)
             child = StorageContainer(header, nodes)
-        elif header.storage_type == StorageType.VALUE:
+        #  elif header.storage_type == StorageType.VALUE:
+        elif header.storage_type.is_value():
+            print(pad(), "value")
             val = read_value(stream, header.length)
             child = StorageValue(header, val)
         else:
             raise Exception(
-                "Unknown header type: {}".format(header.storage_type))
-        childs.append(child)
+                "Unknown header type: {}".format(header.storage_type)
+            )
+        childs.append(attr.asdict(child))
         consumed = stream.tell() - start
+    nest -= 1
     return childs
 
 
-def storage_parse(max_fname, stream_name):
+def read_stream(max_fname, stream_name):
+    """Read the stream and return its contents as bytes.
+    """
     ole = olefile.OleFileIO(max_fname)
     ba = ole.openstream(stream_name).read()
     ole.close()
+    return ba
+
+
+def storage_parse(max_fname, stream_name):
+    ba = read_stream(max_fname, stream_name)
     stream = io.BytesIO(ba)
-    header = None
     nodes = read_container(stream, len(ba))
-    c = StorageContainer(header, nodes)
-    return c
+    return nodes
 
 
 def extract_vpq(max_fname):
@@ -135,7 +231,7 @@ def main():
     max_fname = sys.argv[1]
     container = extract_vpq(max_fname)
     #  print(attr.asdict(container))
-    print(json.dumps(attr.asdict(container), indent=4, cls=EnumEncoder))
+    print(json.dumps(container, indent=4, cls=EnumEncoder, sort_keys=True))
 
 
 if __name__ == "__main__":
--- run.py
+++ run.py
@@ -2,9 +2,10 @@ import argparse
 import json
 
 import attr
+import hexdump
 
 from max_dump.file_props_parser import extract_file_props
-from max_dump.storage_parser import storage_parse, EnumEncoder
+from max_dump.storage_parser import storage_parse, read_stream
 
 
 STREAM_NAMES = ('ClassData', 'ClassDirectory3', 'Config', 'DllDirectory',
@@ -13,9 +14,19 @@ STREAM_NAMES = ('ClassData', 'ClassDirectory3', 'Config', 'DllDirectory',
 
 
 def dumps(c):
-    d = attr.asdict(c)
-    s = json.dumps(d, indent=4, cls=EnumEncoder)
-    return s
+    return json.dumps(c, indent=4)
+
+
+def dump_stream(max_fname, stream_name):
+    """Print contents of the stream as hex string.
+    """
+    ba = read_stream(max_fname, stream_name)
+    print(hexdump.dump(ba))
+
+
+def parse_stream(max_fname, stream_name):
+    c = storage_parse(max_fname, stream_name)
+    print(dumps(c))
 
 
 def main():
@@ -24,14 +35,17 @@ def main():
     )
     parser.add_argument('max_fname')
     help = "Parse chunk-based stream"
-    parser.add_argument('-s', '--parse-stream', choices=STREAM_NAMES,
+    parser.add_argument('-p', '--parse-stream', choices=STREAM_NAMES,
+                        metavar='STREAM_NAME', help=help)
+    help = "Print contents of the stream as hex string"
+    parser.add_argument('-d', '--dump-stream', choices=STREAM_NAMES,
                         metavar='STREAM_NAME', help=help)
     args = parser.parse_args()
 
     if args.parse_stream:
-        stream_name = args.parse_stream
-        c = storage_parse(args.max_fname, stream_name)
-        print(dumps(c))
+        parse_stream(args.max_fname, args.parse_stream)
+    elif args.dump_stream:
+        dump_stream(args.max_fname, args.dump_stream)
     else:
         props = extract_file_props(args.max_fname)
         out = json.dumps(props, indent=4)
