9a0c9cb96188a644239ea4702a7b622c1be6888c
nrdhm <nortis@yandex.ru>
2017-09-17 20:12:06 +0800

    [Utils] Add functions to utils.py and create tests.

---
+++ max_dump/tests/test_utils.py
@@ -0,0 +1,108 @@
+import io
+import unittest
+
+
+from max_dump import utils
+
+
+class UtilsTest(unittest.TestCase):
+    def test_read_short(self):
+        size = utils.SHORT_S
+        n = 42
+        n_bytes = n.to_bytes(size, 'little', signed=True)
+        r = utils.read_short(io.BytesIO(n_bytes))
+        self.assertEqual(n, r)
+
+    def test_read_int(self):
+        size = utils.INT_S
+        n = 42
+        n_bytes = n.to_bytes(size, 'little', signed=True)
+        r = utils.read_short(io.BytesIO(n_bytes))
+        self.assertEqual(n, r)
+
+    def test_read_long_long(self):
+        size = utils.LONG_LONG_S
+        n = 42
+        n_bytes = n.to_bytes(size, 'little', signed=True)
+        r = utils.read_short(io.BytesIO(n_bytes))
+        self.assertEqual(n, r)
+
+    def test_unset_sign_bit_short(self):
+        size = utils.SHORT_S
+        a = -42
+        b = utils.unset_sign_bit(a, size)
+        self.assertEqual(b, 32726)
+        a_hex = a.to_bytes(size, 'big', signed=True).hex()
+        b_hex = b.to_bytes(size, 'big', signed=True).hex()
+        self.assertEqual(a_hex, 'ffd6')
+        self.assertEqual(b_hex, '7fd6')
+
+    def test_unset_sign_bit_int(self):
+        size = utils.INT_S
+        a = -42
+        b = utils.unset_sign_bit(a, size)
+        self.assertEqual(b, 2147483606)
+        a_hex = a.to_bytes(size, 'big', signed=True).hex()
+        b_hex = b.to_bytes(size, 'big', signed=True).hex()
+        self.assertEqual(a_hex, 'ffffffd6')
+        self.assertEqual(b_hex, '7fffffd6')
+
+    def test_unset_sign_bit_long(self):
+        size = utils.LONG_LONG_S
+        a = -42
+        b = utils.unset_sign_bit(a, size)
+        self.assertEqual(b, 9223372036854775766)
+        a_hex = a.to_bytes(size, 'big', signed=True).hex()
+        b_hex = b.to_bytes(size, 'big', signed=True).hex()
+        self.assertEqual(a_hex, 'ffffffffffffffd6')
+        self.assertEqual(b_hex, '7fffffffffffffd6')
+
+    def test_index_by(self):
+        l = [{"id": 1, "age": 30}, {"id": 2, "age": 31}, ]
+        k = utils.index_by(l, "id")
+        res = {1: {'age': 30, 'id': 1}, 2: {'age': 31, 'id': 2}}
+        self.assertEqual(k , res)
+
+    def test_index_by_nested(self):
+        l = [{"id": 1, "age": 30, "person":{"name": "Mike"}},
+             {"id": 2, "age": 31, "person":{"name": "Bob"}}, ]
+        k = utils.index_by(l, "person__name")
+        res = {'Bob': {'age': 31, 'id': 2, 'person': {'name': 'Bob'}},
+               'Mike': {'age': 30, 'id': 1, 'person': {'name': 'Mike'}}}
+        self.assertEqual(k , res)
+
+    def test_index_by_overwrite_key(self):
+        l = [{"id": 1, "age": 30},
+             {"id": 2, "age": 31},
+             {"id": 1, "age": 32}, ]
+        k = utils.index_by(l, "id")
+        res = {1: {'age': 32, 'id': 1}, 2: {'age': 31, 'id': 2}}
+        self.assertEqual(k , res)
+
+    def test_group_by(self):
+        l = [{"id": 1, "age": 30},
+             {"id": 2, "age": 31},
+             {"id": 1, "age": 32}, ]
+        k = utils.group_by(l, "id")
+        res = {1: [{"id": 1, "age": 30}, {"id": 1, "age": 32}],
+               2: [{'age': 31, 'id': 2}, ]}
+        self.assertEqual(k , res)
+
+    def test_group_by_nested(self):
+        l = [
+                {"id": 1, "age": 30, "person":{"name": "Mike"}},
+                {"id": 2, "age": 31, "person":{"name": "Bob"}},
+                {"id": 3, "age": 32, "person":{"name": "Mike"}},
+        ]
+        k = utils.group_by(l, "person__name")
+        res = {
+                'Mike': [
+                    {"id": 1, "age": 30, "person":{"name": "Mike"}},
+                    {"id": 3, "age": 32, "person":{"name": "Mike"}},
+                ],
+                'Bob': [
+                    {"id": 2, "age": 31, "person":{"name": "Bob"}},
+                ],
+        }
+        self.assertEqual(k , res)
+
--- max_dump/utils.py
+++ max_dump/utils.py
@@ -1,21 +1,51 @@
+"""Small functions, objects and constants used by various modules.
+"""
 import logging
 from collections import defaultdict
 from struct import unpack, calcsize
 
-LONG_LONG_S = calcsize('q')
-INT_S = calcsize('i')
-SHORT_S = calcsize('h')
+SHORT_S = calcsize('h')         # 16
+INT_S = calcsize('i')           # 32
+LONG_LONG_S = calcsize('q')     # 64
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger('max_dump')
+LOGGER = logging.getLogger('max_dump')
+
+
+def read_short(stream):
+    """Read signed short integer (usually 16 bit).
+    """
+    return unpack('h', stream.read(SHORT_S))[0]
 
 
 def read_int(stream):
+    """Read signed integer (usually 32 bit).
+    """
     return unpack('i', stream.read(INT_S))[0]
 
 
 def read_long_long(stream):
+    """Read signed integer (usually 64 bit).
+    """
     return unpack('q', stream.read(LONG_LONG_S))[0]
+
+
+def unset_sign_bit(number, length):
+    """Unset sign bit from a `number'.
+
+    `length' is the size of the number in bytes.
+    """
+    number_of_bits = length * 8
+    # 8bit: 0b1000_0000 = 0x80
+    # 32bit: 0x80_00_00_00
+    sign_bit = 1 << (number_of_bits - 1)
+    # 8bit: 0b1111_1111 = 0xFF
+    # 32bit: 0xFF_FF_FF_FF
+    byte_mask = (1 << number_of_bits) - 1
+    # unset sign bit and keep length under int size
+    number &= ~sign_bit & byte_mask
+    # 8bit: ~sign_bit = 0b0111_1111
+    return number
 
 
 def _new_key(entry, key):
@@ -29,14 +59,6 @@ def index_by(iterable, key):
     """Return a dictionary from the given iterable.
 
     `key' may be nested like that: 'header__idn'
-    >>> l = [{"id": 1, "age": 35}, {"id": 2, "age": 32}, ]
-    >>> index_by(l, "id")
-    {1: {'age': 35, 'id': 1}, 2: {'age': 32, 'id': 2}}
-
-    >>> l = [{"id": 1, "age": 35, "person":{"name": "Mike"}}, {"id": 2, "age": 32, "person":{"name": "Bob"}}, ]
-    >>> index_by(l, "person__name")
-    {'Bob': {'age': 32, 'id': 2, 'person': {'name': 'Bob'}},
-     'Mike': {'age': 35, 'id': 1, 'person': {'name': 'Mike'}}}
     """
     indexed = {}
     for entry in iterable:
@@ -46,10 +68,10 @@ def index_by(iterable, key):
 
 
 def group_by(iterable, key):
+    """Group dictinaries in the iterable by key.
+    """
     grouped = defaultdict(list)
     for entry in iterable:
         new_key = _new_key(entry, key)
         grouped[new_key].append(entry)
     return grouped
-
-
