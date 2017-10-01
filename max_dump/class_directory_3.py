d89faa0d0cb44d1be909b4989a9f928569272598
nrdhm <nortis@yandex.ru>
2017-10-01 20:55:15 +0800

    Add ClassEntry.link that returns ClassEntryLinked
    Given an array of DllEntry, adds information about from what dll the class is
    imported from.

--- max_dump/class_directory_3.py
+++ max_dump/class_directory_3.py
@@ -6,6 +6,7 @@ from enum import Enum
 from struct import unpack
 
 from . import storage_parser as sp
+from . import dll_directory as dld
 from . import utils
 from .abc_decoder import AbstractDecoder
 
@@ -66,6 +67,49 @@ class ClassEntry(sp.StorageContainer):
         inst._nest = container._nest
         return inst
 
+    @property
+    def name(self):
+        assert len(self.childs) == 2
+        return self.childs[1].name
+
+    @property
+    def class_id(self):
+        assert len(self.childs) > 1
+        return self.childs[0].class_id
+
+    @property
+    def super_class_id(self):
+        assert len(self.childs) > 1
+        return self.childs[0].super_class_id
+
+    def link(self, dll_entries: t.List[dld.DllEntry]) -> 'ClassEntryLinked':
+        """Add information about from what dll the class is imported from.
+        """
+        if len(dll_entries) > 0 and isinstance(dll_entries[0], dld.DllHeader):
+            dll_entries.pop(0)
+        dll_index = self.childs[0].dll_index
+
+        assert -2 <= dll_index < len(dll_entries), 'Invalid DLL entry index'
+
+        if dll_index == -1:
+            dll_name = 'builtin'
+            dll_description = 'Built-in type'
+        elif dll_index == -2:
+            dll_name = 'script'
+            dll_description = 'Scripted class'
+        else:
+            dll_entry = dll_entries[dll_index]
+            dll_name = dll_entry.name
+            dll_description = dll_entry.description
+
+        return ClassEntryLinked(
+            name=self.name,
+            class_id=self.class_id,
+            super_class_id=self.super_class_id,
+            dll_name=dll_name,
+            dll_description=dll_description,
+        )
+
     def __repr__(self):
         class_name = self.__class__.__name__
         ext = ("ext" if self.header.extended else "")
@@ -94,3 +138,32 @@ class ClassDecoder(AbstractDecoder):
     def _raise_unknown_id_exc(idn):
         raise sp.StorageException("Unknown Class storage id: 0x{:x}"
                                   .format(idn))
+
+
+class ClassEntryLinked:
+    def __init__(
+            self,
+            name: str,
+            class_id: t.Tuple[int, int],
+            super_class_id: int,
+            dll_name: str = '',
+            dll_description: str = ''
+    ) -> None:
+        self.name = name
+        self.class_id = class_id
+        self.super_class_id = super_class_id
+        self.dll_name = dll_name
+        self.dll_description = dll_description
+
+    def __repr__(self):
+        class_name = self.__class__.__name__
+        format_s = f"[{class_name} {self.name}]"
+        props = [
+                self.dll_name,
+                self.dll_description,
+                f'class_id: (0x{self.class_id[0]:x}, 0x{self.class_id[1]:x})',
+                f'super_class_id: 0x{self.super_class_id:x}',
+        ]
+        props_s = '\n'.join(textwrap.indent(str(prop), " " * 4)
+                            for prop in props)
+        return f"{format_s}\n{props_s}\n"
--- todo.md
+++ todo.md
@@ -2,8 +2,24 @@
 
   + Write tests for class_directory_3
 
-  * Create a class ClassEntryLinked that will bind info about ClassEntry that can be obtained from array of DllEntries
+  + Create a class ClassEntryLinked that will bind info about ClassEntry that
+  can be obtained from array of DllEntries
 
 ClassHeader.dll_index is the index of DllEntry containing the class.
 
-  * Add method .link() to ClassEntry that should return instance of ClassEntryLinked.
+  + Add method .link() to ClassEntry that should return instance of ClassEntryLinked.
+
+01.10.17
+
+  - Write tests for ClassEntry.link
+
+  - Remove redundant attributes from derived classes
+
+  ClassName does not need to hold the original binary value, as well as
+  the nesting level, idn, length, storage_type and so on.
+
+  Every derived class from class_directory_3 and dll_directory has this
+  problem.
+
+  It may be interesting to measure the execution time of tests before and after
+  the task is completed.
